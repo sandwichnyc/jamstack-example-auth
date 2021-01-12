549e96c08e97c16147f017e5f120ae339eb9672d
sandwichnyc <svillani722@gmail.com>
2021-01-11 21:53:24 -0500

    add routing stuff

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import './layout.css';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
+
+const Layout = ({ children }) => (
+    <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com">
+        <header>
+            <Link to="/">JAMStack App</Link>
+        </header>
+        <main>{ children }</main>
+    </IdentityContextProvider>
+);
+
+export default Layout;
